ee5e0eb5971f14d7cce8949d65617c058a57008d
Aram Barnett <aram.barnett@gmail.com>
2018-03-07 15:46:25 -0500

    Update kg.cpp

--- kg.cpp
+++ kg.cpp
@@ -3,20 +3,20 @@
 #include <string>
 using namespace std;
 
-string r_Output = "K";
+string r_Output = "";
 
 int
 main ()
 {
   constexpr char chset[] =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    "ABC";
   auto r_char =
     [ =, r_gen { std::mt19937 {std::random_device {} ()} }] ()mutable {
     return chset[std::uniform_int_distribution <> {0, sizeof chset - 1}
 (r_gen)];
   };
 
-  for (auto i = 0; i < 52; ++i)
+  for (auto i = 0; i < 3; ++i)
     r_Output += r_char ();
 
   std::cout << r_Output;
